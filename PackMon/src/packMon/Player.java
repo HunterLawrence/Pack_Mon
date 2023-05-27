a578ee595cf95cbd44a0407acc0bad707d764e00
Tony <sonictony@gmail.com>
2023-05-26 16:25:49 -0800

    Added the Player class

---
+++ PackMon/src/packMon/Player.java
@@ -0,0 +1,76 @@
+package packMon;
+
+//Importing the random class for the attack method
+import java.util.Random;
+
+/*
+ * Player class for the PackMon game
+ */
+public class Player {
+
+	// Fields
+	private int playerID;
+	private int health;
+	private String name;
+	private String weapon;
+	
+	/*
+	 * Constructor class
+	 */
+	public Player(int id, String pName, String pWeapon, int hp) {
+		playerID = id;
+		name = pName;
+		weapon = pWeapon;
+		health = hp;
+	}
+	
+	/*
+	 * Attack method that determines how much damage the player will inflict
+	 */
+	public int attack() {
+		Random rand = new Random();
+		return rand.nextInt(5,15);
+	}
+	
+	/*
+	 * Heal method that recovers the user's health
+	 */
+	public void heal(int hp) {
+		health += hp;
+	}
+	
+	// Getter methods
+	public int getID() {
+		return playerID;
+	}
+	public String getPlayerName() {
+		return name;
+	}
+	public String getWeapon() {
+		return weapon;
+	}
+	public int getHP() {
+		return health;
+	}
+	
+	// Setter methods
+	public void setID(int newID) {
+		playerID = newID;
+	}
+	public void setPlayerName(String newName) {
+		name = newName;
+	}
+	public void setWeapon(String newWeapon) {
+		weapon = newWeapon;
+	}
+	public void setHealth(int hp) {
+		health = hp;
+	}
+	
+	// toString method
+	public String toString() {
+		return "Player Name: " + getPlayerName() + "\nWeapon: " + getWeapon() +
+				"\nPlayer ID: " + getID();
+	}
+	
+}
